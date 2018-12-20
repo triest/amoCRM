f4bff0fb665501db6936eff0acdf3316d55edc04
triest <you@example.com>
2018-12-21 00:01:07 +0300

    Create tasks in loop.

--- leads.php
+++ leads.php
@@ -17,7 +17,7 @@ function getLeads()
     выше).
     Следовательно, мы можем заменить ссылку, приведённую выше на одну из следующих, либо скомбинировать параметры так, как Вам
     необходимо. */
-   // $link = 'https://' . $subdomain . '.amocrm.ru/api/v2/leads?limit_rows=50';
+    // $link = 'https://' . $subdomain . '.amocrm.ru/api/v2/leads?limit_rows=50';
     $link = 'https://' . $subdomain . '.amocrm.ru/api/v2/leads?limit_rows=50&filter[tasks]=1'; //сделки без задач
     /* Следующий запрос вернёт список сделок, у которых есть почта 'test@mail.com' */
     // $link='https://'.$subdomain.'.amocrm.ru/api/v2/leads?query=test@mail.com';
@@ -68,11 +68,108 @@ function getLeads()
      */
 
     $json_output = json_decode($out, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    //
     var_dump($json_output);
-    return $json_output;
 
+    return $json_output;
 }
 
+
+function getIdList($response)
+{
+    $response = $response["_embedded"];
+    $response = $response["items"];
+    // var_dump($response);
+    //  die();
+    $ids = [];
+    foreach ($response as $item) {
+        array_push($ids, $item['id']);
+    }
+    return $ids;
+}
+
+//создает задачу для сделка с вфодным id
+function createTask($id, $name, $end_data, $responsible_user_id)
+{
+
+
+    $tasks['add'] = array(
+        #Привязываем к сделке
+        array(
+            'element_id' => $id, #ID сделки
+            'element_type' => 2, #Показываем, что это - сделка, а не контакт
+            'task_type' => 1, #Звонок
+            'text' => $name,
+            'responsible_user_id' => $responsible_user_id,
+            'complete_till_at' => $end_data
+        ),
+
+    );
+    /* Теперь подготовим данные, необходимые для запроса к серверу */
+    $subdomain = 'triest21'; #Наш аккаунт - поддомен
+    #Формируем ссылку для запроса
+    $link = 'https://' . $subdomain . '.amocrm.ru/api/v2/tasks';
+    /* Нам необходимо инициировать запрос к серверу. Воспользуемся библиотекой cURL (поставляется в составе PHP). Подробнее о
+    работе с этой
+    библиотекой Вы можете прочитать в мануале. */
+    $curl = curl_init(); #Сохраняем дескриптор сеанса cURL
+    #Устанавливаем необходимые опции для сеанса cURL
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+    curl_setopt($curl, CURLOPT_URL, $link);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($tasks));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+    curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+    $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    /* Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+    $code = (int)$code;
+    $errors = array(
+        301 => 'Moved permanently',
+        400 => 'Bad request',
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not found',
+        500 => 'Internal server error',
+        502 => 'Bad gateway',
+        503 => 'Service unavailable'
+    );
+    try {
+        #Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
+        if ($code != 200 && $code != 204) {
+            throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
+        }
+    } catch (Exception $E) {
+        die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
+    }
+}
+
+//получает массив слелок и вызывает функцию создания задач
+function CreateTaskInLoop($array, $userId, $complete_till_at)
+{
+    foreach ($array as $item) {
+        createTask($item, 'сделка без задачи', $complete_till_at, $userId);
+    }
+}
+
+
 $response = getLeads();
-echo $response;
+
+
+// тут загоняем в массив id сделок, без задачь (просто извлекаем id из jsona
+
+$userId = 23393644;
+
+$arrayIDs = getIdList($response);
+$timestamp = strtotime("22-12-2018 15:00");
+
+$date = "22-12-2018 15:00";
+$dtime = DateTime::createFromFormat("d/m/y G:i", "25/12/2018 15:00");
+
+CreateTaskInLoop($arrayIDs, $userId, $dtime);
+
+//triest user id 23393644
